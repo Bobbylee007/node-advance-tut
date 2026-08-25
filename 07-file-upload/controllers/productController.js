ebc9d8abf88f83dd881a68207cc7d3125555cbf5
Bobbylee007 <bobbyleeaustine@gmail.com>
2026-08-25 10:47:59 +0100

    fetching data

--- 07-file-upload/controllers/productController.js
+++ 07-file-upload/controllers/productController.js
@@ -5,12 +5,11 @@ const createProducts = async (req, res) => {
   console.log(req.body);
  const product= await Product.create(req.body) 
  res.status(StatusCodes.CREATED).json({product})
-
- 
 };
 
 const getAllProducts = async (req, res) => {
-  res.send("list of product");
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({products, nbHits: products.length});
 };
 
 module.exports = {createProducts, getAllProducts };
--- 07-file-upload/public/browser-app.js
+++ 07-file-upload/public/browser-app.js
@@ -1,65 +1,88 @@
-// const product = require("../models/product");
+const url = "/api/v1/products";
 
-const url='/api/v1/products'
-
-const fileFormDOM = document.querySelector('.file-form')
-const nameInputDOM = document.querySelector("#name")
-const priceInputDOM = document.querySelector("#price")
+const fileFormDOM = document.querySelector(".file-form");
+const nameInputDOM = document.querySelector("#name");
+const priceInputDOM = document.querySelector("#price");
 const imageInputDOM = document.querySelector("#image");
-const containerDOM = document.querySelector('.container')
+const containerDOM = document.querySelector(".container");
 const cardsDOM = document.querySelector(".cards");
-
 
 let imageValue;
 
+imageInputDOM.addEventListener("change", async (e) => {
+  //anytime there is a change in this element we make a http call to send the element data to server
+  const imageFile = e.target.files[0]; // get the target element
+  const formData = new FormData(); // get the form data and invoke
+  formData.append("image", imageFile); // parse the new file to formdata object 'image' as property name
 
-//retriving data from server
-const showProducts = async()=>{
-    try{
-        const response = await axios.get(url)
-        console.log(response); //full response
-        console.log(response.data);
-                
-    }catch(error){
-        console.log(error);
-        
-    }
-}
-showProducts()
-
-
-imageInputDOM.addEventListener('change', async (e)=>{ //anytime there is a change in this element we make a http call to send the element data to server
-    const imageFile = e.target.files[0]                // get the target element
-   const formData = new FormData()              // get the form data and invoke
-   formData.append('image', imageFile)          // parse the new file to formdata object 'image' as property name
-    
-   try {
-    const {data:{image:{src}}} = await axios.post(`${url}/uploads`, formData,{   // look for src add the data to server
-        headers:{
-            'content-type':'multipart/form-data'    //set the data headers
-        }
-    })
-    imageValue = src;                                //add the scr from server to JS
-   } catch (error) {
-    imageValue = src                                //set the image value null
-    imageValue = null                               
+  try {
+    const {
+      data: {
+        image: { src },
+      },
+    } = await axios.post(`${url}/uploads`, formData, {
+      // look for src add the data to server
+      headers: {
+        "content-type": "multipart/form-data", //set the data headers
+      },
+    });
+    imageValue = src; //add the scr from server to JS
+  } catch (error) {
+    imageValue = null; //set the image value null
     console.log(error);
-   }
-})
+  }
+});
 
 // send data to server
-fileFormDOM.addEventListener('submit', async (e)=>{
-    e.preventDefault()
-    const nameValue = nameInputDOM.value
-    const priceValue = priceInputDOM.value
-   
-    try {
-        const product = {name:nameValue,price:priceValue,image:imageValue} //object to sent to server
-        await axios.post(url, product)  //http call to sent data to server
-        showProducts();               //fetch send data to server to be display after data it sent
-    } catch (error) {
-        
-    }
+fileFormDOM.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const nameValue = nameInputDOM.value;
+  const priceValue = priceInputDOM.value;
 
-})
+  try {
+    const product = { name: nameValue, price: priceValue, image: imageValue }; //object to sent to server
+    await axios.post(url, product); //http call to sent data to server
+    showProducts(); //fetch send data to server to be display after data it sent
+  } catch (error) {}
+});
 
+//retriving data from server
+const showProducts = async () => {
+  // loading is true
+  try {
+    //loading false
+
+    // for return array of products
+    //   const response = await axios.get(url);
+    //   console.log(response); //full response
+    //   console.log(response.data); // Get only data
+
+    //   const products = response.data; //or  const products = response.data.products: for array like data
+    //check if products is less then 1 then (no item)
+
+    const {
+      data: { products },
+    } = await axios.get(url); //destructuring the data from response
+    const productsDOM = products
+      .map((product) => {
+        const { image, price, name } = product;
+        return `
+          <article  class=card>
+            <header>
+            <img src='${image}'class='img' alt='${name}' />
+            </header>
+
+            <footer class='flex-contentInline'>
+            <p>${name}</p>
+            <p>${price}</p>
+            </footer>
+            </article>
+            `;
+      })
+      .join("");
+    cardsDOM.innerHTML = productsDOM;
+  } catch(error){
+    console.log(error);
+  }
+};
+showProducts();
